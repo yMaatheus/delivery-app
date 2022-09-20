63894e06e662f434cdc71bbbc41c89bb6be41f6b
cassiusbessa <cassiusbessa@gmail.com>
2022-09-20 17:41:31 -0300

    build(Sequelize): :tada: creates all models

---
+++ back-end/src/database/models/sale.js
@@ -0,0 +1,36 @@
+'use strict';
+//JSdocs
+  /**
+   * @param {import('sequelize').Sequelize } sequelize 
+   * @param {import('sequelize').DataTypes} DataTypes 
+   */
+const saleModel = (sequelize, DataTypes) => {
+  const Sale = sequelize.define('Sale', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    userId: {type: DataTypes.INTEGER, allowNull: false, foreignKey: true},
+    sellerId: {type: DataTypes.INTEGER, allowNull: false, foreignKey: true},
+    totalPrice: DataTypes.DECIMAL(9, 2),
+    deliveryAdress: DataTypes.STRING,
+    deliveryNumber: DataTypes.STRING,
+		saleDate: DataTypes.DATE,
+		status: DataTypes.STRING,
+  }, {
+    tableName: 'sales',
+    timestamps: true,
+    createdAt: 'saleDate',
+    updatedAt: false,
+    underscored: true,
+  });
+  Sale.associate = (models) => {
+    Sale.belongsTo(models.User,
+      { foreignKey: 'userId', as: 'user' } );      
+  };
+  Sale.associate = (models) => {
+    Sale.belongsTo(models.User,
+      { foreignKey: 'sellerId', as: 'seller' } );
+  };
+  return Sale;
+};
+
+module.exports = saleModel
+
---
+++ back-end/src/database/models/saleProduct.js
@@ -0,0 +1,32 @@
+'use strict';
+//JSdocs
+  /**
+   * @param {import('sequelize').Sequelize } sequelize 
+   * @param {import('sequelize').DataTypes} DataTypes 
+   */
+module.exports = (sequelize, DataTypes) => {
+  const SaleProduct = sequelize.define('SaleProduct', {
+    saleId: DataTypes.INTEGER,
+    productId: DataTypes.INTEGER,
+    quantity: DataTypes.INTEGER,
+  }, {
+    tableName: 'salesProducts',
+    timestamps: false,
+		underscored: true,
+  });
+	SaleProduct.associate = (models) => {
+    models.Product.belongsToMany(models.Sale, {
+      as: 'sales',
+      through: SaleProduct,
+      foreignKey: 'saleId',
+      otherKey: 'productId',
+    });
+    models.Sale.belongsToMany(models.Product, {
+      as: 'products',
+      through: SaleProduct,
+      foreignKey: 'productId',
+      otherKey: 'saleId',
+    });
+  };
+  return SaleProduct;
+};
