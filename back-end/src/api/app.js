86729b2489beb2f80feb9906affebb420a657680
cassiusbessa <cassiusbessa@gmail.com>
2022-09-20 22:23:02 -0300

    build: :rocket: swagger instaled

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../../swagger_output.json');
+const routes = require('./routes/routes');
 const user = require('./routes/user.route');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(routes);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
