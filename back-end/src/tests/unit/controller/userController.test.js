e59127dec77f55b8ad4b3758809a2b5bb5dcfa1c
Matheus <stvgamer74@gmail.com>
2022-09-23 15:25:33 -0300

    test(Tests): :white_check_mark: update tests to new context login to users

--- back-end/src/tests/unit/controller/userController.test.js
+++ back-end/src/tests/unit/controller/userController.test.js
@@ -1,22 +1,22 @@
 const { expect } = require("chai");
 const sinon = require("sinon");
 const { StatusCodes } = require("http-status-codes");
-const LoginService = require('../../../services/login/LoginService');
-const LoginController = require("../../../api/controllers/login/LoginController");
+const UserService = require('../../../services/user/UserService');
+const UserController = require("../../../api/controllers/user/UserController");
 const loginSchema = require("../../../schemas/loginSchema");
 const { userMock, userValidMock, userMockWithId } = require('../../mocks/userMock');
 const { User } = require('../../../database/models');
 const Jwt = require("../../../providers/Jwt");
 
 describe('Login Controller', () => {
-  const loginService = new LoginService();
-  const loginController = new LoginController();
+  const userService = new UserService();
+  const userController = new UserController();
   
   const req = {};
   const res = {};
 
   beforeEach(() => {
-    sinon.stub(loginService, 'login').resolves(userMock);
+    sinon.stub(userService, 'login').resolves(userMock);
 
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
@@ -30,7 +30,7 @@ describe('Login Controller', () => {
       sinon.stub(User, 'findOne').resolves(userMockWithId);
       sinon.stub(Jwt, 'sign').returns('TOKEN_VALIDO');
       req.body = { email: userValidMock.email, password: userValidMock.password };
-      await loginController.login(req, res);
+      await userController.login(req, res);
 
       expect((res.status).calledWith(StatusCodes.OK)).to.be.true;
       expect((res.json).calledWith({ role: 'administrator', token: 'TOKEN_VALIDO' })).to.be.true;
--- back-end/src/tests/unit/services/userService.test.js
+++ back-end/src/tests/unit/services/userService.test.js
@@ -1,25 +1,74 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const { User } = require('../../../database/models');
-const { userMockWithId, userValidMock, userMockSequelize } = require('../../mocks/userMock');
+const { userMockWithId, userMockSequelize } = require('../../mocks/userMock');
 const Jwt = require('../../../providers/Jwt');
 const AppError = require('../../../providers/AppError');
 const UserService = require('../../../services/user/UserService');
 const userSchema = require('../../../schemas/userSchema');
 const { StatusCodes } = require('http-status-codes');
+const loginSchema = require('../../../schemas/loginSchema');
 
 describe('User Service', () => {
   const userService = new UserService();
 
   afterEach(() => sinon.restore())
 
+  describe('Login', () => {
+    it('Success', async () => {
+      sinon.stub(loginSchema, 'validate').resolves();
+      sinon.stub(UserService, 'hashPassword').returns('fakePassword');
+      sinon.stub(User, 'findOne').resolves(userMockWithId);
+      sinon.stub(Jwt, 'sign').returns('TOKEN_VALIDO');
+
+      const result = await userService.login();
+
+      expect(result).to.be.have.property('role');
+      expect(result).to.be.have.property('token');
+    })
+
+    describe('Failure', () => {
+      it('Validate error', async () => {
+        sinon.stub(loginSchema, 'validate').returns({ error: true });
+        let error;
+        try {
+          await userService.login();
+        } catch (err) {
+          error = err
+        }
+
+        expect(error).to.be.instanceOf(AppError);
+        expect(error.statusCode).to.be.equal(StatusCodes.BAD_REQUEST);
+      })
+
+      it('User create error', async () => {
+        sinon.stub(loginSchema, 'validate').returns({ error: false });
+        sinon.stub(UserService, 'hashPassword').returns('fakePassword');
+        sinon.stub(User, 'findOne').resolves(null);
+
+        let error;
+        try {
+          await userService.login();
+        } catch (err) {
+          error = err
+        }
+
+        expect(error).to.be.instanceOf(AppError);
+        expect(error.message).to.be.equal('Invalid email or password');
+        expect(error.statusCode).to.be.equal(StatusCodes.UNAUTHORIZED);
+      })
+
+    })
+  })
+
   describe('Create', () => {
     it('Success', async () => {
       sinon.stub(userSchema, 'validate').resolves();
+      sinon.stub(UserService, 'hashPassword').returns('fakePassword');
       sinon.stub(User, 'findOrCreate').resolves([userMockSequelize, true]);
       sinon.stub(Jwt, 'sign').returns('TOKEN_VALIDO');
 
-      const result = await userService.create({ password: userValidMock.password });
+      const result = await userService.create({});
 
       expect(result).to.be.have.property('id');
       expect(result).to.be.have.property('name');
@@ -44,11 +93,12 @@ describe('User Service', () => {
 
       it('Email not found or password not match', async () => {
         sinon.stub(userSchema, 'validate').returns({ error: false });
+        sinon.stub(UserService, 'hashPassword').returns('fakePassword');
         sinon.stub(User, 'findOrCreate').resolves([null, false]);
 
         let error;
         try {
-          await userService.create({ password: userValidMock.password });
+          await userService.create({ });
         } catch (err) {
           error = err
         }
