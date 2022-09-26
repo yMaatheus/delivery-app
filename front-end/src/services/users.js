24cefa89a61013c316e42b5dc12f3e9c63ed83e1
Lucas Alves <lucasdolohov@gmail.com>
2022-09-26 15:05:45 -0300

    fix route to register

--- front-end/src/services/users.js
+++ front-end/src/services/users.js
@@ -4,4 +4,4 @@ export const getMe = () => api.get('/users/me');
 
 export const login = (data) => api.post('/login', data);
 
-export const register = (data) => api.post('/users', data);
+export const register = (data) => api.post('/users', { ...data, role: 'customer' });
