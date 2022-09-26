0d146efab326471e6306fda18cdacc0c19f295ee
Matheus <stvgamer74@gmail.com>
2022-09-26 20:38:11 -0300

    feat: :sparkles: add customer page and list all products

--- front-end/src/screens/Customer.js
+++ front-end/src/screens/Customer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import NavBar from '../components/NavBar/NavBar';
 import ProductList from '../components/ProductList';
-import NavBar from '../components/NavBar';
 
 function Customer() {
   return (
