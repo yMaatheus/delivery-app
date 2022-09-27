bb30b9e319b3846e8cfc770b7b775073e055c518
Andre <andre360.ars@gmail.com>
2022-09-27 20:29:07 -0300

    feat(Customer): :sparkles: Creates a table of the orders of a client

---
+++ front-end/src/screens/CustomerCheckout.js
@@ -0,0 +1,13 @@
+import NavBar from '../components/NavBar/NavBar';
+import OrdersTable from '../components/OrdersTable/OrdersTable';
+
+function CustomerCheckout() {
+  return (
+    <>
+      <NavBar />
+      <OrdersTable />
+    </>
+  );
+}
+
+export default CustomerCheckout;
