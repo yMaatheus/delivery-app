df2e9c0be82de50fda5e5eceebdd8fab6b085cc2
Matheus <stvgamer74@gmail.com>
2022-09-28 16:48:22 -0300

    fix: :truck: change name AuthenyicatedApp to AuthenticatedApp

--- front-end/src/AuthenticatedApp.js
+++ front-end/src/AuthenticatedApp.js
@@ -1,5 +1,6 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { useUser } from './context/user-context';
+import Admin from './screens/admin';
 import Customer from './screens/Customer';
 import CustomerCheckout from './screens/CustomerCheckout';
 import OrdersDetails from './screens/OrdersDetails';
@@ -16,9 +17,11 @@ function AuthenticatedApp() {
       <Route exact path="/customer/orders/:id" component={ OrdersDetails } />
       <Route exact path="/seller/orders" component={ Seller } />
       <Route exact path="/seller/orders/:id" component={ OrdersDetails } />
+      <Route exact path="/admin/manage" component={ Admin } />
       {
         (user?.role === 'seller' && <Redirect to="/seller/orders" />)
         || (user?.role === 'customer' && <Redirect to="/customer/products" />)
+        || (user?.role === 'administrator' && <Redirect to="/admin/manage" />)
       }
     </Switch>
   );
