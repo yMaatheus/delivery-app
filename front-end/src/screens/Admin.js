d5fef9afa8dc7d900ef2ce95e7fea8d47abab5a4
Matheus <stvgamer74@gmail.com>
2022-09-28 17:31:34 -0300

    refactor: :recycle: move users state to admin screen

--- front-end/src/screens/Admin.js
+++ front-end/src/screens/Admin.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from '../components/NavBar/NavBar';
 import UserForm from '../components/UserForm/UserForm';
 import UserList from '../components/UserList/UserList';
+import { getAllUsers } from '../services/admin';
 
 function Admin() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function getUsers() {
+      const response = await getAllUsers();
+      setUsers(response);
+    }
+    getUsers();
+  }, []);
+
   return (
     <>
       <NavBar />
-      <UserForm />
-      <UserList />
+      <UserForm setUsers={ setUsers } />
+      <UserList users={ users } />
     </>
   );
 }
