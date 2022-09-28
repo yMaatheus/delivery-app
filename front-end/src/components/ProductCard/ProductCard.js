58f5378589375e2a8c08b775b4bb00daa03e7af1
Matheus <stvgamer74@gmail.com>
2022-09-28 00:27:02 -0300

    feat: :sparkles: add product list and add cart button with total price

--- front-end/src/components/ProductCard/ProductCard.js
+++ front-end/src/components/ProductCard/ProductCard.js
@@ -1,71 +1,65 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, ButtonGroup, TextField } from '@mui/material';
+import useProductsStore from '../../store/productStore';
 
 function ProductCard({ id, name, price, urlImage }) {
   const [quantity, setQuantity] = useState(0);
+  const [cart, setCart] = useProductsStore((state) => [state.cart, state.setCart]);
 
   const handleQuantity = (value) => {
     const re = /[0-9]+/g;
-    return (re.test(+value)) && setQuantity(+value);
+    const qtt = +value;
+    if (!re.test(qtt)) return;
+    const product = { id, name, price, urlImage, quantity: qtt };
+    setQuantity(qtt);
+    if (qtt <= 0) {
+      setCart([...cart.filter((item) => item.id !== id)]);
+      return;
+    }
+    setCart([...cart.filter((item) => item.id !== id), { ...product }]);
   };
 
   const handleChange = ({ target: { value } }) => handleQuantity(value);
   const handleIcrement = () => handleQuantity(quantity + 1);
-  const handleDecrement = () => (quantity > 0) && (handleQuantity(quantity - 1));
+  const handleDecrement = () => (quantity > 0 && handleQuantity(quantity - 1));
 
   return (
-    <Card>
-      <CardMedia
-        component="img"
-        height="500"
-        image={ urlImage }
+    <div>
+      <img
+        src={ urlImage }
         alt={ name }
-        sx={ { width: '70%' } }
         data-testid={ `customer_products__img-card-bg-image-${id}` }
       />
-      <CardContent>
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-          data-testid={ `customer_products__element-card-title-${id}` }
-        >
-          {name}
-        </Typography>
-        <Typography
-          variant="body2"
-          color="text.secondary"
+      <div>
+        <h3 data-testid={ `customer_products__element-card-title-${id}` }>{ name }</h3>
+        <p
           data-testid={ `customer_products__element-card-price-${id}` }
         >
-          {price}
-        </Typography>
-
-        <ButtonGroup size="large" aria-label="small outlined button group">
-          <Button
+          { price.replace(/\./, ',') }
+        </p>
+        <div>
+          <button
+            type="button"
             onClick={ handleDecrement }
             data-testid={ `customer_products__button-card-rm-item-${id}` }
           >
             -
-          </Button>
-          <TextField
+          </button>
+          <input
             onChange={ handleChange }
             value={ quantity }
             data-testid={ `customer_products__input-card-quantity-${id}` }
           />
-          <Button
+          <button
+            type="button"
             onClick={ handleIcrement }
             data-testid={ `customer_products__button-card-add-item-${id}` }
           >
             +
-          </Button>
-        </ButtonGroup>
-      </CardContent>
-    </Card>
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- front-end/src/store/productStore.js
+++ front-end/src/store/productStore.js
@@ -3,6 +3,8 @@ import create from 'zustand';
 const useProductsStore = create((set) => ({
   products: [],
   setProducts: (products) => set({ products }),
+  cart: [],
+  setCart: (cart) => set({ cart }),
 }));
 
 export default useProductsStore;
