501d869ffb7655b4ffb2a7d8c99359e7f0eb8d83
Matheus <stvgamer74@gmail.com>
2022-09-29 11:24:14 -0300

    feat: :sparkles: add data-testid and configure custom error message in user create form

--- front-end/src/services/admin.js
+++ front-end/src/services/admin.js
@@ -1,13 +1,5 @@
 import api from './api';
 
-<<<<<<< HEAD
 export const getAllUsers = () => api.get('/admin/users');
 
-export const createUser = (data) => api.post('/admin/users', {
-=======
-export const getAll = () => api.get('/admin/users');
-
-export const create = (data) => api.post('/admin/users', {
->>>>>>> feat: :sparkles: add screen admin and components user list and user form
-  ...data, role: 'administrator',
-});
+export const createUser = (data) => api.post('/admin/users', data);
