50e30d9f1eab9952ecc1ea29d28f5efe98193465
cassiusbessa <cassiusbessa@gmail.com>
2022-09-29 17:06:04 -0300

    fix(Sales): :bug: now getDetails returns product price

--- back-end/src/database/queries/getSaleDetails.sql
+++ back-end/src/database/queries/getSaleDetails.sql
@@ -1,10 +1,10 @@
-SELECT sales.id, users.name as customer, delivery_address, delivery_number, seller.name as seller, JSON_ARRAYAGG(JSON_OBJECT('name', products.name, 'quantity', sp.quantity)) AS products
+SELECT sales.id, users.name as customer, sales.status, sales.sale_date as date, delivery_address, delivery_number, seller.name as seller, JSON_ARRAYAGG(JSON_OBJECT('name', products.name, 'price', products.price, 'quantity', sp.quantity)) AS products
 FROM `delivery-app-dev`.sales sales
 INNER JOIN `delivery-app-dev`.users users
 ON sales.user_id = users.id
 INNER JOIN `delivery-app-dev`.users seller
 ON sales.seller_id = seller.id
-INNER JOIN `delivery-app-dev`.salesProducts sp
+INNER JOIN `delivery-app-dev`.sales_products sp
 ON sp.sale_id = sales.id
 INNER JOIN `delivery-app-dev`.products products
 ON sp.product_id = products.id
